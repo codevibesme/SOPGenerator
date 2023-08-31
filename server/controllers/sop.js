a411b62e1f832100b05b4e3b016cc73683c1a00c
Soham <sohammalakarofficial@gmail.com>
2023-09-01 01:48:53 +0530

    Created SOP pdf in backend and sent it via attachment to recipent

--- server/controllers/sop.js
+++ server/controllers/sop.js
@@ -1,8 +1,13 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import PDFDocument from "pdfkit";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
@@ -11,7 +16,7 @@ const generateSOP = async (prompt) => {
         const completion = await openai.completions.create({
             model: "text-davinci-003",
             prompt,
-            max_tokens: 4096-306,
+            max_tokens: 4096-319,
             temperature: 1,
         });
         const { text } = completion.choices[0];
@@ -70,9 +75,9 @@ export const sendSop = async (req, res) => {
             Did I do a GIC: ${gic}
             How much did I pay towards GIC:  ${gic_fee}
 
-            Write the letter in proper format officially, make the letter long with relevant details and atmost 3 paragraphs.
+            Write the letter in proper format officially, make the letter long with relevant details and atmost 3 paragraphs. Be humble and describe each information in detail so that I surely get the visa.
         `;
-
+        // Setting up Nodemailer to send emails from backend
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -81,19 +86,47 @@ export const sendSop = async (req, res) => {
             },
         });
 
+        // generating prompt answer
         const text = (await generateSOP(prompt)).toString();
+
+        // generating pdf
+        const doc = new PDFDocument({size: 'A4'});
+        doc.pipe(fs.createWriteStream(path.join(__dirname, `../public/assets/${full_name}_sop.pdf`)));
+        doc
+            .font('Times-Roman')
+            .fontSize(16)
+            .text(text);
+        doc.save();
+        doc.end();
         console.log(text);
         if(text!=='null'){
-        const mailOptions = {
-            from: process.env.email,
-            to: email_address,
-            subject: "SOP LETTER",
-            text,
+
+            // Creating mail options by setting up reciever details and message
+            const mailOptions = {
+                from: process.env.email,
+                to: email_address,
+                subject: "SOP LETTER",
+                text: "Please find the attachment of your SOP letter below",
+                attachments: [
+                    {
+                        filename: `${full_name}_sop.pdf`,
+                        path: path.join(__dirname, `../public/assets/${full_name}_sop.pdf`),
+                        contentType: 'application/pdf',
+                    },
+                ],
             }
+
+            // sending the actual mail
             transporter.sendMail(mailOptions, (error, info) => {
                 if(error) console.log(error);
                 else console.log(info);
             });
+
+            // deleting the created file
+            // fs.unlink(path.join(__dirname, `../public/assets/${full_name}_sop.pdf`), err => {
+            //     if(err) console.log(err.message);
+            //     else console.log("Mail sent and pdf deleted from storage");
+            // })
             return res.status(201).json({message: "Sent"}); 
         } else return res.status(400).json({message: "Couldn't generate SOP"});
         
